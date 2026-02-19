4a0e48cb7fec1edb8daef68eacaed768d7c4bb3b
Daniel Campos <danielcampos.santos@ucsal.edu.br>
2026-02-19 17:13:39 -0300

    estrutura basica: innitial cmmit for the basic structure of the project

---
+++ anime-service/src/test/java/io/github/danielcampossantos/AnimeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.danielcampossantos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
