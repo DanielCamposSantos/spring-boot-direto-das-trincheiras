6aba9082c398001d1425ec74ef4532b795f6b3f7
Daniel Campos <danielcampos.santos@ucsal.edu.br>
2026-03-11 20:36:30 -0300

    feat: adição de um handler global para tratamento de SQLIntegrityConstraintViolationException

--- commons-core/src/main/java/io/github/danielcampossantos/exception/GlobalErrorHandlerAdvice.java
+++ commons-core/src/main/java/io/github/danielcampossantos/exception/GlobalErrorHandlerAdvice.java
@@ -5,12 +5,20 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.sql.SQLIntegrityConstraintViolationException;
+
 @RestControllerAdvice
 public class GlobalErrorHandlerAdvice {
 
     @ExceptionHandler(BadRequestException.class)
     public ResponseEntity<DefaultErrorMessage> handleBadRequestException(BadRequestException e) {
         var error = new DefaultErrorMessage(HttpStatus.BAD_REQUEST.value(), e.getReason());
+        return ResponseEntity.badRequest().body(error);
+    }
+
+    @ExceptionHandler(SQLIntegrityConstraintViolationException.class)
+    public ResponseEntity<DefaultErrorMessage> handleSQLIntegrityConstraintViolationException(SQLIntegrityConstraintViolationException e) {
+        var error = new DefaultErrorMessage(HttpStatus.BAD_REQUEST.value(), "Duplicate entry for one other unique fields");
         return ResponseEntity.badRequest().body(error);
     }
 }
