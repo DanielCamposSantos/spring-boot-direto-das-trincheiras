d4d2a841ae73e18b8aac1718f3a1c11ceb5f47da
Daniel <danielcampos.santos1603@gmail.com>
2026-06-25 19:39:49 -0300

    fix: Troca de role para authority e ajuste na configuração de segurança

--- user-service/src/main/java/io/github/danielcampossantos/config/SecurityConfig.java
+++ user-service/src/main/java/io/github/danielcampossantos/config/SecurityConfig.java
@@ -1,15 +1,11 @@
 package io.github.danielcampossantos.config;
 
+import org.jspecify.annotations.NonNull;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
@@ -18,28 +14,6 @@ import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
 @Configuration
 public class SecurityConfig {
     private static final String[] WHITE_LIST = {"/swagger-ui.html", "/swagger-ui/**", "/v3/api-docs/**", "/csrf"};
-
-    @Bean
-    public UserDetailsService userDetailsService(PasswordEncoder encoder) {
-        var user = User
-                .withUsername("user")
-                .password(encoder.encode("password"))
-                .roles("USER")
-                .build();
-
-        var admin = User
-                .withUsername("admin")
-                .password(encoder.encode("admin"))
-                .roles("ADMIN")
-                .build();
-
-        return new InMemoryUserDetailsManager(user, admin);
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
-    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) {
@@ -51,12 +25,16 @@ public class SecurityConfig {
 
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(WHITE_LIST).permitAll()
-                        .requestMatchers(HttpMethod.GET, "/v1/users").hasRole("ADMIN")
-                        .requestMatchers(HttpMethod.POST, "/v1/users").permitAll()
-                        .requestMatchers(HttpMethod.DELETE, "/v1/users").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.GET, getPath()).hasAuthority("ADMIN")
+                        .requestMatchers(HttpMethod.POST, getPath()).permitAll()
+                        .requestMatchers(HttpMethod.DELETE, getPath()).hasAuthority("ADMIN")
                         .anyRequest().authenticated())
                 .httpBasic(Customizer.withDefaults())
                 .build();
+    }
+
+    private static @NonNull String getPath() {
+        return "/v1/users";
     }
 
 }
